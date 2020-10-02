6515372e19add3ae1f25788fe99b0860bc145770
Marc Steele <marcsteele2014@gmail.com>
2020-10-02 23:05:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def using_include(array, element)
-array.include?(element)
+  array.include?(element)
 end
 
 def using_sort(array)
@@ -15,9 +15,9 @@ def using_first(array)
 end
 
 def using_last(array)
-
+  array.last()
 end
 
 def using_size(array)
-
+  array.size()
 end
